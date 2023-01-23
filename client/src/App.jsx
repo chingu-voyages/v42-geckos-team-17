c1f8e0ddddbef3a69124136e97036f36c5a064f7
Jane Moroz <Moroz.jane@yahoo.com>
2023-01-23 18:20:24 +0300

    feat: add color variables

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './styles/App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          type="adder"
-          onClick={() => setCount((prevCount) => prevCount + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Okane</h1>
     </div>
   )
 }
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: {
+          100: '#525252',
+          200: '#000000',
+        },
+        accent1: '#000000',
+        accent2: '#000000',
+        bg1: '#FAFAFA',
+        bg2: '#F2F2F2',
+      },
+    },
   },
   plugins: [],
 }
