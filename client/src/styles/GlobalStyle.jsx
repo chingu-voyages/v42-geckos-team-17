983fe4d78ca7870c9812e5e8af4414446d1c60f9
Jane Moroz <Moroz.jane@yahoo.com>
2023-01-24 14:16:00 +0300

    style: add typography global styles

--- client/src/styles/GlobalStyle.jsx
+++ client/src/styles/GlobalStyle.jsx
@@ -3,7 +3,19 @@ import tw, { GlobalStyles as BaseStyles } from 'twin.macro'
 
 const CustomStyles = createGlobalStyle({
   body: {
-    ...tw`bg-amber-50`,
+    ...tw`bg-white font-body`,
+  },
+  h1: {
+    ...tw`text-[70px] font-extrabold text-text-200`,
+  },
+  h2: {
+    ...tw`text-5xl font-bold text-text-200`,
+  },
+  h3: {
+    ...tw`text-[32px] font-medium text-text-200`,
+  },
+  p: {
+    ...tw`text-lg font-normal text-text-100`,
   },
 })
 
