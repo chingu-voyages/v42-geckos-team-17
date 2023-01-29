ce6ad8cd3b428fbdb3993c98a7ba9fa055bec3ec
JaneMoroz <moroz.jane@yahoo.com>
2023-01-29 20:18:45 +0300

    style: make button/link a bit round

--- client/src/styles/LinkStyles.jsx
+++ client/src/styles/LinkStyles.jsx
@@ -10,7 +10,7 @@ export const TextLink = styled(Link)(({ small }) => [
 ])
 
 export const OutlineLink = styled(Link)`
-  ${tw`bg-white text-accent1 text-lg font-normal whitespace-nowrap py-3 px-10 shadow-button hover:bg-accent1 hover:text-white transition duration-300`}
+  ${tw`bg-white text-accent1 text-lg font-normal whitespace-nowrap py-3 px-10 shadow-button rounded-md hover:bg-accent1 hover:text-white transition duration-300`}
 `
 
 // Icon link
