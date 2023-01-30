b18f4c3ee33586a79c35e88175d16b120ecf5bff
Jane Moroz <Moroz.jane@yahoo.com>
2023-01-30 16:00:55 +0300

    refactor: refactor components index (components call other components directly 😵)

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -6,23 +6,4 @@ import Hero from './home/Hero'
 import Features from './home/Features'
 import Downloads from './home/Downloads'
 
-import FormInputGroup from './form/FormInputGroup'
-import FormCheckGroup from './form/FormCheckGroup'
-import FormSelectGroup from './form/FormSelectGroup'
-
-import ContactInfo from './contact/ContactInfo'
-import ContactForm from './contact/ContactForm'
-
-export {
-  HeaderNav,
-  Footer,
-  SharedLayout,
-  Hero,
-  Features,
-  Downloads,
-  FormInputGroup,
-  FormCheckGroup,
-  FormSelectGroup,
-  ContactForm,
-  ContactInfo,
-}
+export { HeaderNav, Footer, SharedLayout, Hero, Features, Downloads }
