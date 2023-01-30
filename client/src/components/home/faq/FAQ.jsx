dcc18215deff7995853629b2ddfcaa625dbfe8b0
Mohit Ramani <ramanimohit01@gmail.com>
2023-01-30 21:14:24 +0530

    add FAQ and FAQBlock Component

---
+++ client/src/components/home/faq/FAQ.jsx
@@ -0,0 +1,27 @@
+import { Container } from '../../../styles/LayoutStyles'
+import { FAQGrid, FAQInner, FAQSection } from '../../../styles/home/FAQStyles'
+import { faqData } from '../../../assets/data/faqData'
+import FAQBlock from './FAQBlock'
+
+function FAQ() {
+  return (
+    <FAQSection>
+      <Container>
+        <FAQInner>
+          <h1>Frequently asked questions</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <FAQGrid>
+            {faqData.map((obj) => (
+              <FAQBlock question={obj.question} answer={obj.answer} />
+            ))}
+          </FAQGrid>
+        </FAQInner>
+      </Container>
+    </FAQSection>
+  )
+}
+
+export default FAQ
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -5,6 +5,7 @@ import SharedLayout from './sharedLayout/SharedLayout'
 import Hero from './home/Hero'
 import Features from './home/Features'
 import Downloads from './home/Downloads'
+import FAQ from './home/faq/FAQ'
 
 import FormInputGroup from './form/FormInputGroup'
 import FormCheckGroup from './form/FormCheckGroup'
@@ -17,6 +18,7 @@ export {
   Hero,
   Features,
   Downloads,
+  FAQ,
   FormInputGroup,
   FormCheckGroup,
   FormSelectGroup,
