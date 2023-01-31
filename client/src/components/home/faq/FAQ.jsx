3577b163c4ca58904c74e8a859962f0d61d6071c
JaneMoroz <moroz.jane@yahoo.com>
2023-01-31 21:57:33 +0300

    refactor: add section heading and changed width for small screens

--- client/src/components/home/faq/FAQ.jsx
+++ client/src/components/home/faq/FAQ.jsx
@@ -1,18 +1,32 @@
-import { Container } from '../../../styles/LayoutStyles'
+// Styled components
+import {
+  Container,
+  SectionHeadingContainer,
+} from '../../../styles/LayoutStyles'
+import { SecondaryHeading, Paragraph } from '../../../styles/TypographyStyles'
 import { FAQGrid, FAQInner, FAQSection } from '../../../styles/home/FAQStyles'
-import { faqData } from '../../../assets/data/faqData'
+
+// Components
 import FAQBlock from './FAQBlock'
+
+// Data
+import { faqData } from '../../../assets/data/faqData'
 
 function FAQ() {
   return (
     <FAQSection>
       <Container>
         <FAQInner>
-          <h1>Frequently asked questions</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          {/* Start: Section heading */}
+          <SectionHeadingContainer center>
+            <SecondaryHeading>Frequently asked questions</SecondaryHeading>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Paragraph>
+          </SectionHeadingContainer>
+          {/* End: Section heading */}
+          {/* Start: FAQ block */}
           <FAQGrid>
             {faqData.map((obj, index) => (
               <FAQBlock
@@ -22,6 +36,7 @@ function FAQ() {
               />
             ))}
           </FAQGrid>
+          {/* End: FAQ block */}
         </FAQInner>
       </Container>
     </FAQSection>
--- client/src/styles/home/FAQStyles.jsx
+++ client/src/styles/home/FAQStyles.jsx
@@ -6,24 +6,14 @@ export const FAQSection = styled.section`
 
 export const FAQInner = styled.div`
   ${tw`flex flex-col items-center h-fit gap-y-6`}
-
-  // Heading
-  h1 {
-    ${tw`text-5xl text-center `}
-  }
-
-  // Paragraph
-  p {
-    ${tw`lg:max-w-[540px] block text-xl text-center `}
-  }
 `
 
 export const FAQGrid = styled.div`
-  ${tw`grid grid-cols-1 gap-2`}
+  ${tw`grid grid-cols-1 gap-2 justify-items-center w-full`}
 `
 
 export const FAQDiv = styled.div`
-  ${tw`flex flex-col md:justify-between border-2 rounded min-h-[80px] p-4 h-fit w-[342px] md:w-[754px] lg:w-[800px] border-accent1`}
+  ${tw`flex flex-col justify-center md:justify-between border-2 bg-white rounded-md min-h-[80px] p-4 h-fit w-full lg:w-[800px] border-accent1`}
 
   ${({ isOpen }) =>
     isOpen &&
@@ -35,7 +25,7 @@ export const FAQDiv = styled.div`
 
 export const QuestionDiv = styled.div`
   p {
-    ${tw`font-semibold text-left w-fit text-text-200`}
+    ${tw`font-semibold text-left w-fit`}
   }
   svg {
     ${tw`w-[38px] h-[35px] ml-auto mr-0 my-auto`}
@@ -44,6 +34,6 @@ export const QuestionDiv = styled.div`
 
 export const AnswerDiv = styled.div`
   p {
-    ${tw`mt-4 text-xl font-normal text-left text-text-200`}
+    ${tw`mt-4 text-left`}
   }
 `
