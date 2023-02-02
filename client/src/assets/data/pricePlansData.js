defa03b5266ac83853113facfbaafdf8dbcd08d2
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-02 22:54:22 +0530

    add price plane data

---
+++ client/src/assets/data/pricePlansData.js
@@ -0,0 +1,51 @@
+export const pricePlaneData = [
+  {
+    type: 'Premium',
+    description:
+      'Advanced features, unlimited users, intelligent content strategy and planning for teams of all sizes.',
+    price: '$1,499',
+    features: [
+      'One or more sites',
+      'Unlimited users',
+      'Additional applications',
+      'Unlimited queries',
+      'Full website inventories',
+      '60+ included Credits',
+    ],
+  },
+  {
+    type: 'Pro',
+    description:
+      'All of the content intelligence and planning that your force-of-one or small team needs.',
+    price: '$499',
+    features: [
+      '1 site',
+      '1-3 users',
+      '4 powerful applications',
+      '100 queries per month',
+      '500 inventoried pages',
+      '12 annual Credits',
+    ],
+  },
+  {
+    type: 'Plus',
+    description:
+      'Find and monitor your gaps and stand out content, optimize your content writing strategy.',
+    price: '$499',
+    features: [
+      '1 site',
+      '1 user',
+      '4 powerful applications',
+      '50 queries per month',
+      '500 inventoried pages',
+      '5 annual Credits',
+    ],
+  },
+  {
+    type: 'Optimize',
+    description:
+      'Our most popular application. Make sure all of your content will perform before publishing.',
+    price: '$79',
+    features: ['1 user', 'Optimize Text Editor', '25 queries per month'],
+  },
+]
