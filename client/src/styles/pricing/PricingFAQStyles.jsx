0238dd728d67e4ebd9e31465a04db24502e0c6bd
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-03 16:38:52 +0530

    add PricingFAQStyles.jsx

---
+++ client/src/styles/pricing/PricingFAQStyles.jsx
@@ -0,0 +1,13 @@
+import tw, { styled } from 'twin.macro'
+
+export const PricingFAQSection = styled.section`
+  ${tw`py-12 h-fit md:py-20`}
+`
+
+export const PricingFAQInner = styled.div`
+  ${tw`flex flex-col items-center h-fit gap-y-6`}
+`
+
+export const PricingFAQGrid = styled.div`
+  ${tw`grid w-full grid-cols-1 gap-2 justify-items-center`}
+`
