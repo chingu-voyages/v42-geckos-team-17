91ce5e99c0fe966fb003b6ed74c644bb71a91c64
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-03 16:37:57 +0530

    add pricing faq data

---
+++ client/src/assets/data/pricingFaqData.js
@@ -0,0 +1,27 @@
+export const pricingFaqData = [
+  {
+    question: 'What is Okane’s cancellation policy??',
+    answer:
+      "You may cancel your Okane subscription or change plans at any time by clicking on the \"cancel subscription\" button on the My Account->Subscription page. Your paid plan will end on the date of your next billing cycle (i.e. the month or year anniversary of whenever you first paid for your subscription's start). We do not offer refunds for partial use, except in the cases of Okane-caused issues with account access or other problems. If this is the case, email us: support@okane.com and we'll look into any troubles you've experienced.",
+  },
+  {
+    question: 'Can I use software to run Okane searches?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit? Lorem ipsum dolor sit amet, consectetur adipiscing elit? adipiscing elit?',
+  },
+  {
+    question: 'How do I switch to a different plan?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit? Lorem ipsum dolor sit amet, consectetur adipiscing elit? adipiscing elit?',
+  },
+  {
+    question: 'Can I subscribe monthly?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit? Lorem ipsum dolor sit amet, consectetur adipiscing elit? adipiscing elit?',
+  },
+  {
+    question: 'Additional billing questions?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit? Lorem ipsum dolor sit amet, consectetur adipiscing elit? adipiscing elit?',
+  },
+]
