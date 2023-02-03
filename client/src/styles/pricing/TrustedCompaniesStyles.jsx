bfc800e6565128ac4634a05a43d401459b0b1f24
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-03 15:54:57 +0530

    add TrustedCompaniesStyles.jsx

---
+++ client/src/styles/pricing/TrustedCompaniesStyles.jsx
@@ -0,0 +1,17 @@
+import tw, { styled } from 'twin.macro'
+
+export const TrustedCompaniesSection = styled.section`
+  ${tw`py-6 bg-bg1 h-fit md:py-12`}
+`
+
+export const CompaniesGrid = styled.div`
+  ${tw`grid grid-cols-2 gap-1 my-8 md:gap-4 lg:grid-cols-3`}
+`
+
+export const CompanyCard = styled.div`
+  ${tw`flex flex-col items-center justify-center w-40 h-24 px-2 py-2 rounded-lg shadow-lg md:h-36 md:w-80`}
+
+  svg {
+    ${tw`w-36 md:w-fit`}
+  }
+`
