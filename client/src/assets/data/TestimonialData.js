359d66e637ccd83da48beba61db9d1f7ec047dd5
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-03 14:20:17 +0530

    add TestimonialsData

---
+++ client/src/assets/data/TestimonialData.js
@@ -0,0 +1,44 @@
+export const TestimonialsData = [
+  {
+    name: 'Logan Anderson',
+    position: 'Analyst',
+    company: 'Coverdale Fury',
+    testimonial:
+      'I used Okane during the research phase of multiple media plans and was able to source lists of podcasts for partnerships and optimize my audience targeting across Facebook, Twitter and Google Ads.',
+  },
+  {
+    name: 'Rob Bonham',
+    position: 'Digital Marketing Success Manager',
+    company: 'Milestone, Inc.',
+    testimonial:
+      'Okane allowed us to see where our most relevant prospects live online. With that data we can now target content to include those social channels/pages/accounts where engagement is most likely.',
+  },
+  {
+    name: 'Justin Adelson',
+    position: 'Founder',
+    company: 'Perfect Pixel Marketing',
+    testimonial:
+      'Leveraging Okane’s data helped me acquire new leads through Facebook ads at 27% of the average cost per lead! And it helped me discover and expand my audiences for clients with niche products and services.',
+  },
+  {
+    name: 'Mark Traphagen',
+    position: 'VP of Product Marketing and Training',
+    company: 'SEO Clarity',
+    testimonial:
+      "Thanks to Okane we found at least 20 influential Twitter accounts we wouldn't have thought of otherwise who signed up for our tool",
+  },
+  {
+    name: 'Daniel Nestle',
+    position: 'VP Marketing Communications',
+    company: 'Mitsubishi Heavy Industries America',
+    testimonial:
+      'We use Okane to identify partnership opportunities for marketing our thought leadership content to niche audiences; to validate our channel and media choices for advertising, and for social media execution.',
+  },
+  {
+    name: 'Bob Goricki',
+    position: 'Director of Digital Marketing',
+    company: 'Marcum LLP',
+    testimonial:
+      "When presented with a new prospect or opportunity with a current client, we use Okane to learn about their target audience, find the best fit for PR/digital advertising opportunities, and explore YouTube/Podcasts (I haven't seen a tool compile those channels in one place).We've used it for multiple new business pitches to suggest industry placements the client should be considering - making us look like we have a deep knowledge of their industry without hours of in-depth research.",
+  },
+]
