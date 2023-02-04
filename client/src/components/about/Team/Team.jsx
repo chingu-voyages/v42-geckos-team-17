cd3386e4e092f5ffd5b95a2967da4d299df556cd
JaneMoroz <moroz.jane@yahoo.com>
2023-02-04 23:22:17 +0300

    feat: add team section

--- client/src/components/about/Team/Team.jsx
+++ client/src/components/about/Team/Team.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Components
+import TeamCard from './TeamCard'
 
 // Styled Components
-import { TeamContainer } from '../../../styles/about/TeamStyles'
+import {
+  TeamContainer,
+  TeamList,
+  ButtonsContainer,
+  RoundButton,
+} from '../../../styles/about/TeamStyles'
+import {
+  Container,
+  SectionHeadingContainer,
+} from '../../../styles/LayoutStyles'
+import { SecondaryHeading, Paragraph } from '../../../styles/TypographyStyles'
+
+// Data
+import teamData from '../../../assets/data/teamData'
 
 function Team() {
-  return <TeamContainer>Team</TeamContainer>
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  // Scrolling handler
+  const handleScroll = (e) => {
+    const screenWidth = window.innerWidth
+    const scrollX = e.currentTarget.scrollLeft
+
+    if (scrollX < screenWidth / 2) {
+      setActiveIndex(0)
+    } else if (scrollX < screenWidth) {
+      setActiveIndex(1)
+    } else if (scrollX < 2 * screenWidth) {
+      setActiveIndex(2)
+    } else if (scrollX < 3 * screenWidth) {
+      setActiveIndex(3)
+    }
+  }
+
+  return (
+    <TeamContainer>
+      <Container>
+        {/* Start: Heading */}
+        <SectionHeadingContainer center>
+          <SecondaryHeading>Meat out team</SecondaryHeading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Paragraph>
+        </SectionHeadingContainer>
+        {/* End: Heading */}
+        {/* Start: Slider */}
+        <TeamList onScroll={handleScroll}>
+          {teamData.map((member) => (
+            <TeamCard key={member.name} member={member} />
+          ))}
+        </TeamList>
+        {/* End: Slider */}
+        {/* Start: Buttons */}
+        <ButtonsContainer>
+          {teamData.map((_, index) => (
+            <RoundButton
+              isActive={index === activeIndex}
+              key={`member-${index}`}
+            />
+          ))}
+        </ButtonsContainer>
+        {/* End: Buttons */}
+      </Container>
+    </TeamContainer>
+  )
 }
 
 export default Team
--- client/src/styles/about/TeamStyles.jsx
+++ client/src/styles/about/TeamStyles.jsx
@@ -1,5 +1,37 @@
 import tw, { styled } from 'twin.macro'
 
 export const TeamContainer = styled.div`
-  ${tw``}
+  ${tw`py-12 md:py-16 lg:py-20`}
+`
+
+export const TeamList = styled.div`
+  ${tw`mt-10 pt-12 pb-8 grid grid-flow-col gap-6 overflow-x-scroll snap-x snap-mandatory`}
+
+  -ms-scroll-snap-type: x mandatory;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+  scrollbar-width: none; /* Firefox */
+`
+
+export const TeamCardContainer = styled.div`
+  ${tw`relative w-[calc(100vw-48px)] md:w-[550px] pt-12 pb-8 px-8 snap-center flex flex-col items-center gap-y-4 text-center border border-accent1 rounded-md`}
+
+  img {
+    ${tw`h-[72px] w-[72] absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%]`}
+  }
+`
+
+export const SocialLinks = styled.div`
+  ${tw`flex gap-4`}
+`
+
+export const ButtonsContainer = styled.div`
+  ${tw`flex justify-center gap-1 mt-6 sm:hidden`}
+`
+// `
+
+export const RoundButton = styled.div`
+  ${tw`inline w-4 h-4 bg-white shadow-button rounded-full transition delay-300`}
+
+  ${({ isActive }) => isActive && tw`bg-accent1`}
 `
