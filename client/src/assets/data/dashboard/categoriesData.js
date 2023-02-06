efbed2093587b72c1f4a3419c74a1fbb78d59047
JaneMoroz <moroz.jane@yahoo.com>
2023-02-06 18:24:36 +0300

    feat: add categories data

---
+++ client/src/assets/data/dashboard/categoriesData.js
@@ -0,0 +1,83 @@
+// Icons
+import {
+  TransportIcon,
+  ShoppingIcon,
+  GroceriesIcon,
+  CoursesIcon,
+  GoingOutIcon,
+  TravelIcon,
+  CashIcon,
+  BeautyIcon,
+  DecorIcon,
+  OtherIcon,
+  AccountsIcon,
+} from '../../icons/dashboardIcons'
+
+const categories = [
+  {
+    id: 'transport',
+    name: 'transport',
+    icon: <TransportIcon />,
+    color: '#C7F9CC',
+  },
+  {
+    id: 'shopping',
+    name: 'shopping',
+    icon: <ShoppingIcon />,
+    color: '#FFD6FF',
+  },
+  {
+    id: 'groceries',
+    name: 'groceries',
+    icon: <GroceriesIcon />,
+    color: '#A8DADC',
+  },
+  {
+    id: 'courses',
+    name: 'courses',
+    icon: <CoursesIcon />,
+    color: '#FFDDD2',
+  },
+  {
+    id: 'goingOut',
+    name: 'goingOut',
+    icon: <GoingOutIcon />,
+    color: '#FF8FA3',
+  },
+  {
+    id: 'travel',
+    name: 'travel',
+    icon: <TravelIcon />,
+    color: '#ddb892',
+  },
+  {
+    id: 'cash',
+    name: 'cash',
+    icon: <CashIcon />,
+    color: '#ECE184',
+  },
+  {
+    id: 'beauty',
+    name: 'beauty',
+    icon: <BeautyIcon />,
+    color: '#cfbaf0',
+  },
+  {
+    id: 'decor',
+    name: 'decor',
+    icon: <DecorIcon />,
+    color: '#cfe1b9',
+  },
+  {
+    id: 'other',
+    name: 'other',
+    icon: <OtherIcon />,
+    color: '#d6e2e9',
+  },
+  {
+    id: 'account',
+    name: 'account',
+    icon: <AccountsIcon />,
+    color: '#cfdbd5',
+  },
+]
