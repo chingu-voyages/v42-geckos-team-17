dded97de57223cf764733b39373bbbd29fdf0e0b
JaneMoroz <moroz.jane@yahoo.com>
2023-02-06 20:00:11 +0300

    feat: add expense slice

--- client/src/store.js
+++ client/src/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 
 // Slices
 import userSlice from './features/user/userSlice'
+import expenseSlice from './features/expense/expenseSlice'
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
+    expense: expenseSlice,
   },
 })
